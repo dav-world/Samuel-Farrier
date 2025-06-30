35490a4c087bb5d6ea5c05ed242e715f35f79789
David Zheng <zhengdavid94@gmail.com>
2025-06-30 19:59:49 -0400

    fixes

--- sf-nextjs/src/app/(main)/exhibition/[slug]/page.tsx
+++ sf-nextjs/src/app/(main)/exhibition/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import ExhibitionDetailRealtime from "./ExhibitiondetailRealtime";
+import ExhibitionDetailRealtime from "./ExhibitionDetailRealtime";
 
 export default function Page({ params }: { params: { slug: string } }) {
   return <ExhibitionDetailRealtime slug={params.slug} />;
